d5ff0e3a9fae1d6a69b50bb78a827bae5acd14aa
Your Name <you@example.com>
2021-07-17 12:18:41 +0300

    missed

--- scripts/SysinternalsFileDiskUtilities.ps1
+++ scripts/SysinternalsFileDiskUtilities.ps1
@@ -27,9 +27,9 @@ Write-Host "################### accesschk ######################################
 # shows which Windows services members of the Users group have write access to
 # accesschk users -cw *
 #see what Registry keys under HKLM\CurrentUser a specific account has no access to
-accesschk -kns $(whoami) hklm\software 
+# accesschk -kns $(whoami) hklm\software 
 #see the security on the HKLM\Software key
-# accesschk -k hklm\software
+accesschk -k hklm\software
 # see all global objects that Everyone can modify
 # accesschk -wuo everyone \basednamedobjects
 
