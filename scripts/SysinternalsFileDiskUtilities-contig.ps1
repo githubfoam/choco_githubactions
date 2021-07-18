3a5983126e7ad0353993c6de8ac5c56856e505df
Your Name <you@example.com>
2021-07-18 14:10:33 +0300

    init junction

--- scripts/SysinternalsFileDiskUtilities-contig.ps1
+++ scripts/SysinternalsFileDiskUtilities-contig.ps1
@@ -24,26 +24,6 @@ choco install --yes --no-progress --virus-check sysinternals
 
 # https://docs.microsoft.com/en-us/sysinternals/downloads/file-and-disk-utilities
 
-Write-Host "####################################################################"
-Write-Host "################### accesschk ######################################"
-Write-Host "####################################################################"
-
-# https://docs.microsoft.com/en-us/sysinternals/downloads/accesschk
-# reports the accesses that the Power Users account has to files and directories in \Windows\System32
-# accesschk "power users" c:\windows\system32
-# shows which Windows services members of the Users group have write access to
-# accesschk users -cw *
-# see what Registry keys under HKLM\CurrentUser a specific account has no access to
-# accesschk -kns $(whoami) hklm\software 
-# see the security on the HKLM\Software key
-# accesschk -k hklm\software
-# see all files under \Users\Mark on Vista that have an explicit integrity level
-# accesschk -e -s c:\users\$env:UserName
-# see all global objects that Everyone can modify
-# accesschk -wuo everyone \basednamedobjects
-# see all global objects that currentuser can modify
-# accesschk -wuo $env:UserName \basednamedobjects
-
 Write-Host "####################################################################" -ForegroundColor red -BackgroundColor white
 Write-Host "################### contig #########################################" -ForegroundColor red -BackgroundColor white
 Write-Host "####################################################################" -ForegroundColor red -BackgroundColor white
@@ -53,6 +33,5 @@ Write-Host "####################################################################
 # # the wildcard symbol * allows whole directories and drives to be defragmented
 contig -s -accepteula C:\* 
 # contig -v -s C:\*
-# # for /R /D %a in (*.*) do start /b contig -q "%a" > nul
 # # When the filesystem is NTFS, contig can also analyse and defragment
-# contig -v -s $mft
+contig -v -s $mft
