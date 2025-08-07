06d4f4e750cd49aa86c657c19c7ce21566c553a0
tester bester <your.email@example.com>
2025-08-07 08:52:47 +0300

    debug

--- scripts/Choco_Packages_Desktop_windows10.ps1
+++ scripts/Choco_Packages_Desktop_windows10.ps1
@@ -9,69 +9,69 @@ Write-Output $VerbosePreference
 # https://docs.chocolatey.org/en-us/choco/commands/install
 choco upgrade chocolatey #Installing choco updates
 
-Write-Host "####################################################################"
-Write-Host "###################      system tools          #####################"
-Write-Host "####################################################################"
-choco install --yes --no-progress --virus-check sysinternals `
-    psscriptanalyzer osquery ChocolateyGUI packer `
-    terraform vagrant virtualbox  vmware-workstation-player 
+# Write-Host "####################################################################"
+# Write-Host "###################      system tools          #####################"
+# Write-Host "####################################################################"
+# choco install --yes --no-progress --virus-check sysinternals `
+#     psscriptanalyzer osquery ChocolateyGUI packer `
+#     terraform vagrant virtualbox  vmware-workstation-player 
     
-Write-Host "####################################################################"
-Write-Host "###################      net tools            #####################"
-Write-Host "####################################################################"
-choco install --yes --no-progress --virus-check wireshark network-miner angryip advanced-ip-scanner 
+# Write-Host "####################################################################"
+# Write-Host "###################      net tools            #####################"
+# Write-Host "####################################################################"
+# choco install --yes --no-progress --virus-check wireshark network-miner angryip advanced-ip-scanner 
 
-Write-Host "####################################################################"
-Write-Host "###################      security              #####################"
-Write-Host "####################################################################"
-choco install --yes --no-progress --virus-check zap burp-suite-free-edition fiddler
+# Write-Host "####################################################################"
+# Write-Host "###################      security              #####################"
+# Write-Host "####################################################################"
+# choco install --yes --no-progress --virus-check zap burp-suite-free-edition fiddler
 
-Write-Host "####################################################################"
-Write-Host "###################      browsers              #####################"
-Write-Host "####################################################################"
+# Write-Host "####################################################################"
+# Write-Host "###################      browsers              #####################"
+# Write-Host "####################################################################"
 
-choco install --yes --no-progress --virus-check googlechrome firefox `
-    safari opera tor-browser thunderbird  
+# choco install --yes --no-progress --virus-check googlechrome firefox `
+#     safari opera tor-browser thunderbird  
   
     
-Write-Host "####################################################################"
-Write-Host "###################      tools                 #####################"
-Write-Host "####################################################################"
+# Write-Host "####################################################################"
+# Write-Host "###################      tools                 #####################"
+# Write-Host "####################################################################"
 
-choco install --yes --no-progress --virus-check 7zip etcher  `
-    rufus treesizefree  virtualclonedrive 
+# choco install --yes --no-progress --virus-check 7zip etcher  `
+#     rufus treesizefree  virtualclonedrive 
 
-Write-Host "####################################################################"
-Write-Host "###################      remote conn           #####################"
-Write-Host "####################################################################"
-choco install --yes --no-progress --virus-check putty winscp `
-     mremoteng filezilla deluge utorrent openvpn       
+# Write-Host "####################################################################"
+# Write-Host "###################      remote conn           #####################"
+# Write-Host "####################################################################"
+# choco install --yes --no-progress --virus-check putty winscp `
+#      mremoteng filezilla deluge utorrent openvpn       
 
-Write-Host "####################################################################"
-Write-Host "###################      visual video audio    #####################"
-Write-Host "####################################################################"
-choco install --yes --no-progress --virus-check vlc screamer `
-    googleearth icloud itunes
+# Write-Host "####################################################################"
+# Write-Host "###################      visual video audio    #####################"
+# Write-Host "####################################################################"
+# choco install --yes --no-progress --virus-check vlc screamer `
+#     googleearth icloud itunes
 
-Write-Host "####################################################################"
-Write-Host "###################      catalogues            #####################"
-Write-Host "####################################################################"
+# Write-Host "####################################################################"
+# Write-Host "###################      catalogues            #####################"
+# Write-Host "####################################################################"
 
-choco install --yes --no-progress --virus-check calibre zotero keepass mendeley
+# choco install --yes --no-progress --virus-check calibre zotero keepass mendeley
 
-Write-Host "####################################################################"
-Write-Host "###################      editors               #####################"
-Write-Host "####################################################################"
+# Write-Host "####################################################################"
+# Write-Host "###################      editors               #####################"
+# Write-Host "####################################################################"
 
-choco install --yes --no-progress --virus-check foxitreader `
-    notepadplusplus libreoffice-still
+# choco install --yes --no-progress --virus-check foxitreader `
+#     notepadplusplus libreoffice-still
 
-Write-Host "####################################################################"
-Write-Host "###################      development           #####################"
-Write-Host "####################################################################"
+# Write-Host "####################################################################"
+# Write-Host "###################      development           #####################"
+# Write-Host "####################################################################"
 
-choco install --yes --no-progress --virus-check vscode git tortoisegit `
-    postman PyCharm-community eclipse anaconda3 miniconda3
+# choco install --yes --no-progress --virus-check vscode git tortoisegit `
+#     postman PyCharm-community eclipse anaconda3 miniconda3
 
 Write-Host "####################################################################"
 Write-Host "###################      compilers             #####################"
